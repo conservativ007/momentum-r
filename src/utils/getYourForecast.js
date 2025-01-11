ec8736e1943c51182a40023e1dd4408006b8f4de
Maks <alisa3d2007@gmail.com>
2025-01-11 15:06:12 +0300

    feat: improved function for receiving weatger data

--- src/utils/getYourForecast.js
+++ src/utils/getYourForecast.js
@@ -1,32 +1,53 @@
 import axios from "axios";
 
+const saveDataToLocalStorage = (key, data) => {
+  const timestamp = new Date().getTime();
+  // console.log(timestamp);
+  localStorage.setItem(key, JSON.stringify({ data, timestamp }));
+};
+
+export const getDataFromLocalStorage = (key, maxAgeMs) => {
+  const item = localStorage.getItem(key);
+
+  if (!item) return null;
+
+  const { data, timestamp } = JSON.parse(item);
+
+  const now = new Date().getTime();
+
+  if (now - timestamp > maxAgeMs) {
+    localStorage.removeItem(key);
+    return null;
+  }
+
+  return data;
+};
+
 export const fetchDailyForecast = async (cityName) => {
   if (cityName.length === 0) return;
 
-  const getCityForecast = localStorage.getItem("city-forecast");
-  if (getCityForecast !== null) {
-    console.log("data from locale storage");
-    // console.log(getCityForecast);
-    return JSON.parse(getCityForecast);
+  const getCityForecast = getDataFromLocalStorage("city-forecast", 3600 * 1000); // 1 hour
+  // console.log("getCityForecast", getCityForecast);
+  if (getCityForecast) {
+    console.log("Data from locale storage");
+    return getCityForecast;
   }
 
   console.log("data from API");
   const KEY = "bf677b264ac04013a0990631242512";
+  // const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
+  // console.log("apiKey", apiKey);
   const URL = `https://api.weatherapi.com/v1/forecast.json?key=${KEY}&q=${cityName}&days=18&aqi=no&alerts=no`;
 
   try {
     const response = await axios.get(URL);
 
     const forecastData = response.data.forecast;
-
-    localStorage.setItem("city-forecast", JSON.stringify(forecastData));
-
-    // console.log(response);
+    saveDataToLocalStorage("city-forecast", forecastData);
 
     return forecastData;
-    // console.log(response);
-    // setDays(response.data.daily);
   } catch (error) {
     console.error("Error fetching daily forecast:", error);
+    throw new Error("Unable to fetch weather data. Please try again later.");
   }
 };
